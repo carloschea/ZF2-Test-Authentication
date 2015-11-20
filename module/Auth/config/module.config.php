16ee20f5a90e250c48e858f315764cb84c1cc75a
carloschea <carloshenrique.elias@gmail.com>
2015-11-20 08:32:50 -0300

    change 0.1

--- module/Auth/config/module.config.php
+++ module/Auth/config/module.config.php
@@ -19,7 +19,7 @@ return array(
                     'default' => array(
                         'type' => 'Segment',
                         'options' => array(
-                            'route' => '[/:action[/:id]]',
+                            'route' => '[/:action[/:id]][/:form]]',
                             'constraints' => array(
                                 'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
                                 'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
